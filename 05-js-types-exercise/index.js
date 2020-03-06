8cb90b5ffe014812bf57c98591d8faf1c4031195
Giulia <giuliamummolo@Giulias-MacBook-Pro.local>
2020-03-06 20:26:25 +0000

    exercises 03-07

---
+++ 05-js-types-exercise/index.js
@@ -0,0 +1,22 @@
+var greeting = '';
+var phrase = '${greeting}, ${name}';
+
+var price = 1.1;
+
+var quantity = 5;
+
+var total = price * quantity;
+
+var age1 = 1;
+var age2 = 2;
+var age3 = 3;
+
+var averageAge = ( age1 + age2 + age3 ) / 3;
+
+var loggedIn = true;
+
+var adminUser = false;
+
+var missingInfo = undefined;
+
+
--- 07-js-comparisons-and-conditionals-exercise/index.js
+++ 07-js-comparisons-and-conditionals-exercise/index.js
@@ -7,3 +7,12 @@ const passwordConfirmation = "JSisCool888";
 const sortOrder = "ALPHABETICAL";
 
 // ADD YOUR CODE BELOW
+
+const fruits = 'banana, cherry, watermelon';
+const passwordMessage = 'Oops, your password and confirmation do not match!';
+
+const targetMatchesUserValue = false;
+const passwordMatchesConfirmation = false;
+
+const targetMatchesCoercedUserValue = true;
+
